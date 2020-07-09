b2b70c065614a7f35c82e765723a4077353ef00c
Jon Nguyen <jon.mail@hotmail.com>
2020-07-09 02:11:18 -0700

    can move one room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,24 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import *
+
+class Player:
+    """
+    starts outside room. Name, current room, todo: inventory
+    """
+    def __init__(self, name, current_room, inventory):
+        self.name = name
+        self.room = current_room
+        self.inventory = inventory
+
+    def set_location(self, direction):
+        """
+        update the location based on input, display error if direction not allowed
+        """
+
+        next_room = room[self.room].navigate(direction)
+        if next_room != None:
+            self.room = next_room
+            print(next_room)
+        else:
+            print("You ran into a wall, try another direction!")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,68 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    """
+    Display the  room and potentially the items
+    """
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.s_to = 1
+        self.n_to = 1
+        self.e_to = 1
+        self.w_to = 1
+
+    def navigate(self, direction):
+        """
+        take the input and turn it into the room direction attribute
+        """
+        if direction == 'n':
+            return self.n_to
+        
+        elif direction == 's':
+            return self.s_to
+
+        elif direction == 'w':
+            return self.w_to
+        
+        elif direction == 'e':
+            return self.e_to
+        else:
+            return None
+
+    def __str__(self):
+        return f"\n \n{self.name} : \n {self.description}"
+
+# Declare all the rooms
+
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
+
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east."""),
+
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm."""),
+
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
+
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south."""),
+}
+
+
+# Link rooms together
+
+room['outside'].n_to = room['foyer']
+room['foyer'].s_to = room['outside']
+room['foyer'].n_to = room['overlook']
+room['foyer'].e_to = room['narrow']
+room['overlook'].s_to = room['foyer']
+room['narrow'].w_to = room['foyer']
+room['narrow'].n_to = room['treasure']
+room['treasure'].s_to = room['narrow']
